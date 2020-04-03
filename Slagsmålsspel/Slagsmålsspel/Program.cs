0a3cf233b05c5673f91e0f139d219f10bc24b7a7
emjerlin <emma.jerlin@elev.ga.ntig.se>
2020-04-03 18:36:42 +0200

    Sista fixar
    Fixat fighting delen, ändrat så att man inte kan skriva fel karaktär, ändrat loading, fixat bättre användargränssnitt

--- Slagsmålsspel/Slagsmålsspel/Program.cs
+++ Slagsmålsspel/Slagsmålsspel/Program.cs
@@ -12,41 +12,60 @@ namespace Slagsmålsspel
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Welcome to Ultimate Smash Bros! Here, you will battle to get glory, fame, and a bit of coin too! Please write your name to set up your account!");
+            int wins = 0;
 
+            Console.WriteLine("Welcome to Ultimate Smash Bros! Here, you will battle to get glory and fame! Please write your name to set up your account!");
             string name = Console.ReadLine();
             Console.WriteLine("Greetings " + name + ". Enjoy your stay and your future battles. We're starting up the game");
 
             loading();
+
             int restart = 0;
 
             int chosenChamp = 0;
-            while (restart < 1)
+            while (restart < 1) //håller igång spelet tills n skrivs i slutet och då spelet avslutas , restart ökar med 1 och uppfyller inte kraven längre
             {
 
 
-                string[] champion = { "Pikachu", "Princess Peach", "Sonic", "Kirby" };
-                int[] championHP = { 140, 154, 120, 100 };
+                string[] champion = { "Invalid character", "Pikachu", "Princess Peach", "Sonic", "Kirby" };
+                int[] championHP = { 0, 140, 154, 120, 100 };
+                int[] average = { 0, 20, 23, 31, 30 };
 
 
 
                 int repeatChampSelect = 0;
                 while (repeatChampSelect == 0)
                 {
-                    Console.Clear();
-                    Console.WriteLine("Choose your fighter. By picking a fighter, you can then see it's HP, average damage and attacks before confirming your selection:");
-
-                    for (int i = 0; i < 4; i++)
+                    int invalidChampion = 0;
+                    while (invalidChampion == 0)
                     {
-                        Console.WriteLine(champion[i]);
+                        Console.Clear();
+                        Console.WriteLine("Choose your fighter. By picking a fighter, you can then see it's HP and average damage before confirming your selection:");
+                        Console.WriteLine("Your current total wins are " + wins + "!");
+                        Console.WriteLine();
+
+                        for (int i = 1; i < 5; i++)
+                        {
+                            Console.WriteLine(champion[i]);
+                        } //skriver ut karaktärerna i arrayen förutom den första med index 0 då detta är för att kompensera för om personen skriver en ogiltig karaktär. 
+                        //jag använder en for loop för det är samma procedur som ska ske flera gånger, och då istället för att skriva samma writeline 4 gånger med en liten förändring har jag skrivit det i en for loop istället.
+
+                        string champSelect = Console.ReadLine();
+                        Console.WriteLine();
+
+                        chosenChamp = compareChamp(champSelect);
+
+                        if (chosenChamp == 0)
+                        {
+                            Console.WriteLine("Please choose a character from the list!");
+                            loading();
+                        }
+                        else
+                        {
+                            Console.WriteLine("You have chosen " + champion[chosenChamp] + ". This champion has " + championHP[chosenChamp] + " HP and does an average of " + average[chosenChamp] + " damage");
+                            invalidChampion++;
+                        }
                     }
-
-                    string champSelect = Console.ReadLine();
-
-                    chosenChamp = compareChamp(champSelect);
-
-
-                    Console.WriteLine("You have chosen " + champion[chosenChamp] + ". This champion has " + championHP[chosenChamp] + "HP and does an average of __  damage");
 
                     int confirmRepeat = 0;
 
@@ -56,6 +75,7 @@ namespace Slagsmålsspel
                         Console.WriteLine("Do you want to confirm this selection? (y/n)");
 
                         string confirm = Console.ReadLine();
+                        Console.WriteLine();
 
                         if (confirm == "y")
                         {
@@ -78,100 +98,118 @@ namespace Slagsmålsspel
 
                 Console.WriteLine("The match is starting... Searching for an opponent...");
                 loading();
+                Console.Clear();
                 int enemy = enemyGenerator();
-                Console.WriteLine("Enemy found! You are meeting " + champion[enemy] + " in battle. They have " + championHP[enemy] + "HP. Good luck...");
+
+                // match is starting 
+
+                Console.WriteLine("Enemy found! You are meeting " + champion[enemy] + " in battle. They have " + championHP[enemy] + " HP and does an average of " + average[enemy] + " damage. Good luck...");
 
 
-                Random Generator = new Random();
-                int hit = Generator.Next(0, 5);
-                if (hit >= 1)
+                int yourHP = championHP[chosenChamp];
+                int enemyHP = championHP[enemy];
+
+                loading();
+
+                while (enemyHP > 0 && yourHP > 0)
                 {
-                    int amountDMG = attack(chosenChamp);
-                    Console.WriteLine(champion[chosenChamp] + " hits " + champion[enemy] + " for " + amountDMG + " damage!");
+                    Console.Clear();
 
-                    championHP[enemy] = championHP[enemy]-amountDMG;
-                    Console.WriteLine("they are down to" + championHP[enemy] + "HP");
-                }
+                    Random Generator = new Random(); // random generator för både om slaget träffar men också vilken action enemien använder
 
+                    int actionChoose = 0;
 
-
-
-
-                /*
-                int StitchHP = HP;
-
-                int AmaliaHP = HP;
-
-                int end = 0;
-
-                while (end != 1)
-                {
-                    Random Generator = new Random();
-                    if (end != 1)
+                    while (actionChoose == 0)
                     {
-                        int hit = Generator.Next(0, 5); //genererar ett värde för hit, alltså om attacken träffar
-                        int attacks = Generator.Next(1, 30); // genererar ett värde för attacken, alltså hur mycket damage den gör om det träffar
 
-                        if (hit >= 1) //om hit är större eller lika med 1 träffar den
+                        Console.WriteLine("It's your turn:        Attack: A        Heal: H");
+                        string actionWrite = Console.ReadLine();
+                        string action = actionWrite.ToLower();
+
+
+                       
+                        if (action == "a")
                         {
-                            if (AmaliaHP > 0) // om Amalia har mer än 0 hp kommer hon kunna attackera, annars är hon död
+                            int hit = Generator.Next(0, 5);
+                            if (hit >= 1)
                             {
+                                int amountDMG = attack(chosenChamp);
+                                Console.WriteLine(champion[chosenChamp] + " hits " + champion[enemy] + " for " + amountDMG + " damage!");
 
-                                Console.WriteLine("Stitch attacks dealing " + attacks + " damage");
+                                enemyHP -= amountDMG;
+                                Console.WriteLine("Your opponent is down to " + enemyHP + " HP");
 
-                                AmaliaHP = AmaliaHP - attacks;
-
-                                Console.WriteLine("Amalia now has " + AmaliaHP + " hp!");
                             }
+                            else
+                            {
+                                Console.WriteLine("You tried to attack, but missed!");
+                                Console.WriteLine("Your opponent has " + enemyHP + " HP left");
+                            }
+                            actionChoose++;
+
                         }
-                        else //det som händer om hit är under 1
+                        if (action == "h")
                         {
-                            Console.WriteLine("Stitch missed his attack! Amalia still has " +AmaliaHP +" hp!");
-                        }
-                        if (AmaliaHP < 0)
-                        {
-                            Console.WriteLine("Stitch won!");
-                            end = end + 1; // lägger till 1 till end, vilket gör att ingen kan attackera längre, utan spelet avslutas istället
+                            int amountHeal = attack(enemy);
+                            Console.WriteLine("You healed from " + yourHP + " to " + (yourHP + amountHeal) + " HP");
+                            yourHP += amountHeal;
+                            actionChoose++;
                         }
 
+                        if (action != "a" && action != "h")
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Choose from the following actions, please");
+                        }
                     }
-                    if (end != 1)
+
+                    Console.WriteLine("It's the enemies turn. They're picking their action");
+                    loading();
+                    Console.WriteLine();
+
+                    int enemyAction = Generator.Next(1, 6);
+
+                    if (enemyAction <= 3)
                     {
                         int hit = Generator.Next(0, 5);
-
-
-                        int attacka = Generator.Next(1, 30);
-                        Console.ReadLine();
-
                         if (hit >= 1)
                         {
-                            if (StitchHP > 0)
-                            {
+                            int amountDMG = attack(enemy);
+                            Console.WriteLine(champion[enemy] + " hits " + champion[chosenChamp] + " for " + amountDMG + " damage!");
 
-
-                                Console.WriteLine("Amalia attacks dealing " + attacka + " damage");
-
-                                StitchHP = StitchHP - attacka;
-
-                                Console.WriteLine("Stitch now has " + StitchHP + " hp!");
-
-                            }
+                            yourHP -= amountDMG;
+                            Console.WriteLine("You are down to " + yourHP + " HP");
                         }
                         else
                         {
-                            Console.WriteLine("Amalia missed his attack! Stitch still has " + StitchHP + " hp!");
+                            Console.WriteLine("The enemy tried to attack, but missed!");
+                            Console.WriteLine("You have " + yourHP +" HP left");
                         }
-                        if (StitchHP < 0)
-                        {
-                            Console.WriteLine("Amalia won!");
-                            end = end + 1;
-                        }
-                        Console.ReadLine();
                     }
-                }
-                */
+                    if (enemyAction > 3)
+                    {
+                        int amountHeal = attack(chosenChamp);
+                        Console.WriteLine("They healed from " + enemyHP + " to " + (enemyHP + amountHeal) + " HP");
+                        enemyHP += amountHeal;
+                    }
+                    Console.WriteLine("Press enter");
+                    Console.ReadLine();
 
-                Console.WriteLine("End");
+                }
+                if (enemyHP <= 0)
+                {
+                    Console.WriteLine("You won! Claim your price!");
+                    wins++;
+                    Console.WriteLine("Your current total wins are " + wins + "!");
+                }
+                else if (yourHP <= 0)
+                {
+                    Console.WriteLine("You lost...You won't be getting a pocal this time");
+                    Console.WriteLine("Your current total wins are " + wins + "!");
+                }
+                //match is over
+
+
 
                 Console.WriteLine("Would you like to play again? y/n");
 
@@ -188,28 +226,36 @@ namespace Slagsmålsspel
 
 
         static int compareChamp(string champSelect)
+
         {
-            string[] champion = { "Pikachu", "Princess Peach", "Sonic", "Kirby" };
+            string champSelectLow = champSelect.ToLower();
+            string champSelectCompact = champSelectLow.Trim();
+
+
+            string[] champion = { "Invalid character", "pikachu", "princess peach", "sonic", "kirby", };
 
             int champNumber = 0;
 
-            for (int i = 0; i < 4; i++)
+            for (int i = 1; i < 5; i++)
             {
-                if (champSelect == champion[i])
+                if (champSelectCompact == champion[i])
                 {
                     champNumber = i;
                 }
+
             }
+
+
             return champNumber;
 
         }
-        static int enemyGenerator ()
+        static int enemyGenerator()
         {
-            string[] champion = { "Pikachu", "Princess Peach", "Sonic", "Kirby" };
+            string[] champion = { "Invalid Character", "Pikachu", "Princess Peach", "Sonic", "Kirby" };
 
             Random Generator = new Random();
 
-            int enemy = Generator.Next(0, 4);
+            int enemy = Generator.Next(1, 5);
 
             return enemy;
 
@@ -225,19 +271,19 @@ namespace Slagsmålsspel
             {
                 Random generator = new Random();
 
-                if (chosenChamp == 0)
+                if (chosenChamp == 1)
                 {
                     r = generator.Next(15, 26);
                 }
-                else if (chosenChamp == 1)
+                else if (chosenChamp == 2)
                 {
                     r = generator.Next(10, 36);
                 }
-                else if (chosenChamp == 2)
+                else if (chosenChamp == 3)
                 {
                     r = generator.Next(22, 41);
                 }
-                else if (chosenChamp == 3)
+                else if (chosenChamp == 4)
                 {
                     r = generator.Next(10, 51);
                 }
@@ -247,7 +293,7 @@ namespace Slagsmålsspel
         static void loading()
         {
             string[] dots = { "." };
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i <8; i++)
             {
                 Console.Write(dots[0]);
                 System.Threading.Thread.Sleep(500);
